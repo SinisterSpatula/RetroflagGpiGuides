2450dd7de1a9fb2eef0456d6bafcfdb60273a0af
Adam F <49018201+SinisterSpatula@users.noreply.github.com>
2019-08-08 04:36:35 -0500

    Update kernel.sh

--- data/kernel/kernel.sh
+++ data/kernel/kernel.sh
@@ -13,9 +13,9 @@
 sudo wget -O /sr-kernel.tar.gz https://github.com/SinisterSpatula/SuperRetropieGuides/raw/master/data/kernel/sr-kernel.tar.gz
 sudo wget -O /boot/sr-modules.tar.gz https://github.com/SinisterSpatula/SuperRetropieGuides/raw/master/data/kernel/sr-modules.tar.gz
 cd /
-sudo tar -xzvf sr-modules.tar.gz
+sudo tar -xzvf /sr-modules.tar.gz /
 cd /boot
-sudo tar -xzvf sr-kernel.tar.gz
+sudo tar -xzvf /boot/sr-kernel.tar.gz /boot
 echo "--------------------------"
 echo "|| Done.  Rebooting!    ||"
 echo "--------------------------"
