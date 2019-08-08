92b1f66366c26faa32ceb35ba0719e0a07ef9e04
Adam F <49018201+SinisterSpatula@users.noreply.github.com>
2019-08-08 04:29:48 -0500

    Update kernel.sh

--- data/kernel/kernel.sh
+++ data/kernel/kernel.sh
@@ -9,15 +9,7 @@
 #usage:         sudo ./kernel.sh
 #==============================================================================
 
-function validate_url(){
-  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
 
-if validate_url https://github.com/SinisterSpatula/SuperRetropieGuides/raw/master/data/kernel/sr-kernel.tar.gz; then
 sudo wget -O /sr-kernel.tar.gz https://github.com/SinisterSpatula/SuperRetropieGuides/raw/master/data/kernel/sr-kernel.tar.gz
 sudo wget -O /boot/sr-modules.tar.gz https://github.com/SinisterSpatula/SuperRetropieGuides/raw/master/data/kernel/sr-modules.tar.gz
 cd /
@@ -25,15 +17,7 @@ sudo tar -xzvf sr-modules.tar.gz
 cd /boot
 sudo tar -xzvf sr-kernel.tar.gz
 echo "--------------------------"
-echo "|| Success!  Rebooting! ||"
+echo "|| Done.  Rebooting!    ||"
 echo "--------------------------"
 sleep 10s
 sudo reboot
-     else
-    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-    echo ".                                      ."
-    echo ".FAILED! File not available or wifi off."
-    echo ".                                      ."
-    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-    sleep 10s
-    exit
